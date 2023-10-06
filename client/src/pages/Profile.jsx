40d62edc119e49c57815a713aa0cc20da3fc7ef8
mego1907 <ahmedabdelmageed18@gmail.com>
2023-10-06 15:08:26 +0300

    update the header and make the profile page private

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import { useSelector } from "react-redux"
 
 const Profile = () => {
+  const {currentUser} = useSelector(state => state.user);
+
+  console.log(currentUser)
+
   return (
-    <div>Profile</div>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7"></h1>
+    
+      <form className="flex flex-col gap-4">
+        <img 
+          src={currentUser?.avatar} 
+          alt="profile" 
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2" 
+        />
+        <input 
+          type="text" 
+          placeholder="username" 
+          id="username" 
+          className="border p-3 rounded-lg" 
+          value={currentUser?.username}
+        />
+        <input 
+          type="email" 
+          placeholder="email" 
+          id="email" 
+          className="border p-3 rounded-lg" 
+          value={currentUser?.email}
+        />
+        <input 
+          type="password" 
+          placeholder="password" 
+          id="password" 
+          className="border p-3 rounded-lg" 
+          value={currentUser?.password}
+        />
+
+        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+          update
+        </button>
+      </form>
+    </div>
   )
 }
 
