979f104a291f407185661f968ce2ea365e2f7e19
mego1907 <ahmedabdelmageed18@gmail.com>
2023-10-06 16:00:40 +0300

    Complete profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -41,6 +41,11 @@ const Profile = () => {
           update
         </button>
       </form>
+
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign Out</span>
+      </div>
     </div>
   )
 }
